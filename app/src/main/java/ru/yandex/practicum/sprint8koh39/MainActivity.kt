3289125816b69469e0186b374bcfd6ea1e762049
Aleksei Anisimov <anisimov.aleksei.v@gmail.com>
2026-03-31 01:03:21 +0300

    Sprint 8 activity lifecycle

--- app/src/main/java/ru/yandex/practicum/sprint8koh39/MainActivity.kt
+++ app/src/main/java/ru/yandex/practicum/sprint8koh39/MainActivity.kt
@@ -1,14 +1,25 @@
 package ru.yandex.practicum.sprint8koh39
 
+import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "SPRINT_8"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate: $this savedInstanceState=$savedInstanceState")
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -16,5 +27,62 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        findViewById<Button>(R.id.back).setOnClickListener {
+            finish()
+        }
+
+        findViewById<Button>(R.id.start).setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+//            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+        }
+        var s = ""
+        (1..100).forEach {
+           s += Date().toString()
+        }
+
+
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: $this")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: $this")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        Log.d(TAG, "onConfigurationChanged: $newConfig")
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: $this")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: $this")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: $this")
+    }
+
+
 }
